82d3b8363fa57e63762106b8e974a43ac439217d
NickDThompson <nicholat@mtu.edu>
2017-04-19 21:37:31 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ env(__dirname + '/secrets.env');
 // Create simple echo bot
 login({email: process.env.fbUser, password: process.env.fbPass}, function callback (err, api) {
   if(err) return console.error(err);
+
+  api.setOptions({listenEvents: true});
+  api.setOptions({selfListen: false});
+
   api.listen(function callback(err, message) {
-      api.sendMessage(message.body, message.threadID);
+      if(err) return console.log(err);
+
+      switch(message.type) {
+        case "message":
+          api.markAsRead(message.threadID, function(err) {
+            if (err) console.log(err);
+          });
+          api.sendMessage(message.body, message.threadID);
+          console.log(message);
+          break;
+        case "event":
+          console.log(message);
+          break;
+      }
   });
 });
