ff7ad38e381c573b8be49ba2ebcdbe596bce5830
Austin <ajwgeek@gmail.com>
2017-04-20 15:25:08 -0400

    Cleaned up Facebook login.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var login   = require("facebook-chat-api");
 var sqlite3 = require('sqlite3').verbose();
 
 env(__dirname + '/secrets.env'); // Load secret variables from a file.
+
 var db = new sqlite3.Database('games.sqlite'); // Initialize the games database.
+var loginToken = { email: process.env.fbUser,
+  password: process.env.fbPass }; // Holds Facebook login information.
 
 // Create simple echo bot
-login({email: process.env.fbUser, password: process.env.fbPass}, function callback (err, api) {
+login(loginToken, function callback (err, api) {
   if(err) return console.error(err);
 
   api.setOptions({listenEvents: true});
