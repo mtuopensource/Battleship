fd4a543b5728358a6b8f78277885e79a1f269172
Austin <ajwgeek@gmail.com>
2017-04-19 22:18:16 -0400

    Austin Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var env   = require('node-env-file');
 var login = require("facebook-chat-api");
 //This is Isaac's new comment
 env(__dirname + '/secrets.env');
-
+// Austin was here
 // Create simple echo bot
 login({email: process.env.fbUser, password: process.env.fbPass}, function callback (err, api) {
   if(err) return console.error(err);
