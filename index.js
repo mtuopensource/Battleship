083328c03c9d7a20fcc6fadd0797d8b3fc42411d
Austin <ajwgeek@gmail.com>
2017-04-19 21:10:19 -0400

    Added support for secret variables

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var env   = require('node-env-file');
 var login = require("facebook-chat-api");
 
+env(__dirname + '/secrets.env');
+
 // Create simple echo bot
-login({email: "", password: ""}, function callback (err, api) {
+login({email: process.env.fbUser, password: process.env.fbPass}, function callback (err, api) {
   if(err) return console.error(err);
   api.listen(function callback(err, message) {
       api.sendMessage(message.body, message.threadID);
