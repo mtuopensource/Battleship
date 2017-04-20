fecd6bf5e1a4a021bb219d84e34cfa698264eb1e
Austin <ajwgeek@gmail.com>
2017-04-20 17:50:00 -0400

    Split login function into methods

--- index.js
+++ index.js
@@ -7,37 +7,68 @@ env(__dirname + '/secrets.env'); // Load secret variables from a file.
 var db = new sqlite3.Database('games.sqlite'); // Initialize the games database.
 var loginToken = { email: process.env.fbUser,
   password: process.env.fbPass }; // Holds Facebook login information.
-var options = { listenEvents: true,
-  selfListen: false }; // Holds Facebook chat options.
+var loginPrefs = { listenEvents: true,
+    selfListen: false }; // Holds Facebook chat options.
+
+login(loginToken, onLogin);
+
+/**
+ * [onLogin description]
+ * @param  {[type]} err [description]
+ * @param  {[type]} api [description]
+ * @return {[type]}     [description]
+ */
+function onLogin(err, api) {
+  if(err) {
+    return console.error(err);
+  }
+  api.setOptions(loginPrefs);
+  api.listen(function callback(err, message) {
+    onEventReceived(api, err, message);
+  });
+}
+
+/**
+ * [onEventReceived description]
+ * @param  {[type]} api     [description]
+ * @param  {[type]} err     [description]
+ * @param  {[type]} message [description]
+ * @return {[type]}         [description]
+ */
+function onEventReceived(api, err, message) {
+  if(err) {
+    return console.log(err);
+  }
+  switch(message.type) {
+    case 'message':
+      console.log(message);
+      insertMessage(message);
+      api.markAsRead(message.threadID, function(err) {
+        if(err) {
+          console.error(err);
+        }
+      });
+      break;
+    case 'event':
+      console.log(message);
+      break;
+  }
+}
+
+/**
+ * [insertMessage description]
+ * @param  {[type]} message [description]
+ * @return {[type]}         [description]
+ */
+function insertMessage(message) {
+  db.serialize(function() {
+    var stmt = db.prepare("INSERT INTO messages (threadID, messageID, body) VALUES (?, ?, ?)");
+    stmt.run(message.threadID, message.messageID, message.body);
+    stmt.finalize();
+  });
+}
 
 process.on('exit', function() {
   console.log('Goodbye!');
   db.close(); // Cleanup resources we're using and close connections.
-});
-
-// Create a bot that logs all messages
-login(loginToken, function callback (err, api) {
-  if(err) return console.error(err);
-  api.setOptions(options);
-  api.listen(function callback(err, message) {
-      if(err) return console.log(err);
-
-      switch(message.type) {
-        case "message": //If a message is sent to the chat do this:
-          api.markAsRead(message.threadID, function(err) {
-            if (err) console.log(err); //Mark the message as read
-          });
-          //api.sendMessage(message.body, message.threadID); //TODO: echo if we want it
-          db.serialize(function() { //Adds all messages sent to the DB
-            var stmt = db.prepare("INSERT INTO messages (threadID, messageID, body) VALUES (?)");
-            stmt.run(message.threadID, message.messageID, message.body);
-            stmt.finalize();
-          });
-          console.log(message);
-          break;
-        case "event": //Console.log all events
-          console.log(message);
-          break;
-      }
-  });
 });
