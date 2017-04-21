53abe49e504cfa132ffa39057a603947d82bcd2e
Austin <ajwgeek@gmail.com>
2017-04-21 02:18:48 -0400

    Moved game logic to separate file.

---
+++ Game.js
@@ -0,0 +1,189 @@
+var Constants = require('./Constants.js');
+var Random    = require('./Random.js');
+var method    = Game.prototype;
+
+function Game(fbUserID, fbThreadID, fbAPI, id) {
+  this.started = false;
+  this.fbUserID = fbUserID;
+  this.fbThreadID = fbThreadID;
+  this.fbAPI = fbAPI;
+  this.id = id;
+  this.gameBoard = this.createGameBoard();
+  this.gameBoardCPU = this.createGameBoard();
+}
+
+/**
+ * Checks if  (x, y) is valid for a ship of length length.
+ * @param  Number gameBoard  The board which will be checked.
+ * @param  Number length     The length of the ship to be added.
+ * @param  Number x          X coordinate of the ship.
+ * @param  Number y          Y coordinate of the ship.
+ * @return Boolean           True if the position is valid.
+ */
+method.checkShipX = function(gameBoard, length, x, y) {
+  var max = Constants.GAME_BOARD_SIZE - 1;
+  for(var i = 0; i < length; i++) {
+    if(y + i > max || gameBoard[x][y + i] > 0) {
+      return false; // Position out of bounds or intersects another ship.
+    }
+  }
+  return true;
+};
+
+/**
+ * Checks if  (x, y) is valid for a ship of length length.
+ * @param  Number gameBoard  The board which will be checked.
+ * @param  Number length     The length of the ship to be added.
+ * @param  Number x          X coordinate of the ship.
+ * @param  Number y          Y coordinate of the ship.
+ * @return Boolean           True if the position is valid.
+ */
+method.checkShipY = function(gameBoard, length, x, y) {
+  var max = Constants.GAME_BOARD_SIZE - 1;
+  for(var i = 0; i < length; i++) {
+    if(x + i > max || gameBoard[x + i][y] > 0) {
+      return false; // Position out of bounds or intersects another ship.
+    }
+  }
+  return true;
+};
+
+/**
+ * Adds a ship to the gameBoard at (x, y) in a horizontal orientation.
+ * @param Number gameBoard  The board to add the ship to.
+ * @param Number length     The length of the ship.
+ * @param Number x          X coordinate of the ship.
+ * @param Number y          Y coordinate of the ship.
+ */
+method.addShipX = function(gameBoard, length, x, y) {
+  for(var i = 0; i < length; i++) {
+    gameBoard[x][y + i] = length;
+  }
+};
+
+/**
+ * Adds a ship to the gameBoard at (x, y) in a vertical orientation.
+ * @param Number gameBoard  The board to add the ship to.
+ * @param Number length     The length of the ship.
+ * @param Number x          X coordinate of the ship.
+ * @param Number y          Y coordinate of the ship.
+ */
+method.addShipY = function(gameBoard, length, x, y) {
+  for(var i = 0; i < length; i++) {
+    gameBoard[x + i][y] = length;
+  }
+};
+
+/**
+ * Adds a ship to gameBoard at (x, y), if the position is valid.
+ * @param  Number gameBoard  The board to add the ship to.
+ * @param  Number length     The length of the ship.
+ * @param  Number x          X coordinate of the ship.
+ * @param  Number y          Y coordinate of the ship.
+ * @param  Boolean vertical  If true, ship is placed vertically, horizontal otherwise.
+ * @return Boolean           True if the ship was placed, false otherwise.
+ */
+method.addShip = function(gameBoard, length, x, y, vertical) {
+  var valid = false;
+  if(vertical) {
+      valid = this.checkShipY(gameBoard, length, x, y);
+      if(valid) this.addShipY(gameBoard, length, x, y); // We have a valid location, add the ship.
+  } else {
+      valid = this.checkShipX(gameBoard, length, x, y);
+      if(valid) this.addShipX(gameBoard, length, x, y); // We have a valid location, add the ship.
+  }
+  return valid;
+};
+
+/**
+ * Creates a new game board, and initializes all elements to zero.
+ * @return Integer array representing a game board.
+ */
+method.createGameBoard = function() {
+  var x = new Array(Constants.GAME_BOARD_SIZE);
+  for (var i = 0; i < x.length; i++) {
+    x[i] = new Array(Constants.GAME_BOARD_SIZE);
+    x[i].fill(0); // Fill the empty board with zeroes.
+  }
+  return x;
+};
+
+/**
+ * Resets the cpu board and prompts the user for input.
+ */
+method.beginGame = function() {
+  this.addComputerShip(5);
+  this.addComputerShip(4);
+  this.addComputerShip(3);
+  this.addComputerShip(3);
+  this.addComputerShip(2);
+  this.fbAPI.sendMessage('Please place your carrier.', this.fbThreadID);
+};
+
+/**
+ * Adds a ship of given length to a random (x, y) position, with
+ * a random orientation.
+ * @param Number length The length of the ship.
+ */
+method.addComputerShip = function(length) {
+  var addedShip = false;
+  var gameBoard = this.gameBoardCPU;
+  while(addedShip == false) {
+    var x = Random.getRandomInt(0, Constants.GAME_BOARD_SIZE - 1);
+    var y = Random.getRandomInt(0, Constants.GAME_BOARD_SIZE - 1);
+    var v = Random.getRandomInt(0, 1) == 1; // True when ship will be placed vertically.
+    addedShip = this.addShip(gameBoard, length, x, y, v); // Attempt to add the ship.
+  }
+};
+
+/**
+ * Attempts to add a ship of given length and orientation at (x, y)
+ * @param Number    x           X coordinate of the ship.
+ * @param Number    y           Y coordinate of the ship.
+ * @param Boolean   orientation If true, ship is placed vertically, horizontal otherwise.
+ * @param Number    length      The length of the ship.
+ * @param String    name        The name of the ship.
+ * @param String    next        The name of the next ship. If undefined, the game begins after successful placement.
+ */
+Game.prototype.addPlayerShip = function(x, y, orientation, length, name, next) {
+  var ship = this.addShip(this.gameBoard, length, x, y, orientation); // See if it works!
+  if(ship) {
+    if(next) {
+      this.fbAPI.sendMessage('Please place your ' + next + '.', this.fbThreadID); // Successful, prompt for next.
+    } else {
+      this.fbAPI.sendMessage('You go first!', this.fbThreadID); // Successful, game can start now.
+      this.isStarted = true;
+    }
+  } else {
+    this.fbAPI.sendMessage('Invalid coordinates, please place your ' + name + '.', this.fbThreadID); // Bad position given.
+  }
+  return ship;
+}
+
+/**
+ * Processes the last message received, and updates the game state accordingly.
+ * @param  Object message Contains metadata for a message such as date, time, and body.
+ */
+Game.prototype.messageReceive = function(message) {
+  if(this.started) {
+    // TODO: Take turn!
+  } else {
+    var messageSplit = message.split(' ');
+    var x = parseInt(messageSplit[0].charAt(0));
+    var y = parseInt(messageSplit[0].charAt(1));
+    var o = messageSplit[1] == 'vertical';
+    if(!this.carrier) {
+      this.carrier = this.addPlayerShip(x, y, o, 5, 'carrier', 'battleship');
+    } else if(!this.battleship) {
+      this.battleship = this.addPlayerShip(x, y, o, 4, 'battleship', 'cruiser');
+    } else if(!this.cruiser) {
+      this.cruiser = this.addPlayerShip(x, y, o, 3, 'cruiser', 'submarine');
+    } else if(!this.submarine) {
+      this.submarine = this.addPlayerShip(x, y, o, 3, 'submarine', 'destroyer');
+    } else if(!this.destroyer) {
+      this.destroyer = this.addPlayerShip(x, y, o, 2, 'destroyer', '');
+    }
+  }
+};
+
+module.exports = Game;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var Constants = require('./constants.js');
+var Constants = require('./Constants.js');
+var Game      = require('./Game.js');
 var Sqlite3   = require('sqlite3').verbose();
 var env       = require('node-env-file');
 var login     = require("facebook-chat-api");
@@ -12,267 +13,84 @@ env(__dirname + Constants.FILE_PREFS); // Load user preferences from a file.
 
 var db    = new Sqlite3.Database(Constants.FILE_DB); // Load the games database from a file.
 var token = { email: process.env.fbUser, password: process.env.fbPass }; // Facebook login information.
-var prefs = { listenEvents: true, selfListen: false }; // Chat options
+var prefs = { listenEvents: true, selfListen: false }; // Chat preferences
 var games = { }; // A dictionary relating user id to game.
 
-login(loginToken, onLogin); // Create a connection to Facebook.
-
-function Game() {
-  this.isStarted = false;
-  this.opponentID = -1;
-  this.gameID = -1;
-  this.threadID = -1;
-  this.playerGameBoard = createGameBoard();
-  this.computerGameBoard = createGameBoard();
-  this.api = null;
-  this.carrier = false;
-  this.battleship = false;
-  this.cruiser = false;
-  this.submarine = false;
-  this.destroyer = false;
-}
-
-Game.prototype.beginGame = function() {
-  this.addComputerShip(5);
-  this.addComputerShip(4);
-  this.addComputerShip(3);
-  this.addComputerShip(3);
-  this.addComputerShip(2);
-  this.api.sendMessage('Please place your carrier.', this.threadID);
-};
-
-
-Game.prototype.tryPlaceShip = function(x, y, orientation, length, name, next) {
-  var ship = addShip(this.playerGameBoard, length, x, y, orientation);
-  if(ship) {
-    if(next) {
-      this.api.sendMessage('Please place your ' + next + '.', this.threadID);
-    } else {
-      this.api.sendMessage('You go first!', this.threadID);
-      this.isStarted = true;
-    }
-  } else {
-    this.api.sendMessage('Invalid coordinates, please place your ' + name + '.', this.threadID);
-  }
-  return ship;
-}
-
-
-Game.prototype.messageReceive = function(message) {
-  var messageSplit = message.split(" ");
-  var x = parseInt(messageSplit[0].charAt(0));
-  var y = parseInt(messageSplit[0].charAt(1));
-  var o = messageSplit[1] == "vertical";
-
-  if(!this.carrier) {
-    this.carrier = this.tryPlaceShip(x, y, o, 5, 'carrier', 'battleship');
-  } else if(!this.battleship) {
-    this.battleship = this.tryPlaceShip(x, y, o, 4, 'battleship', 'cruiser');
-  } else if(!this.cruiser) {
-    this.cruiser = this.tryPlaceShip(x, y, o, 3, 'cruiser', 'submarine');
-  } else if(!this.submarine) {
-    this.submarine = this.tryPlaceShip(x, y, o, 3, 'submarine', 'destroyer');
-  } else if(!this.destroyer) {
-    this.destroyer = this.tryPlaceShip(x, y, o, 2, 'destroyer', '');
-    console.log(this.playerGameBoard);
-  }
-};
-
-Game.prototype.addComputerShip = function(shipLength) {
-  var added = false;
-  var gameBoard = this.computerGameBoard;
-  while(added == false) {
-    var x = getRandomInt(0, Constants.GAME_BOARD_SIZE - 1);
-    var y = getRandomInt(0, Constants.GAME_BOARD_SIZE - 1);
-    var vertical = getRandomInt(0, 1) == 1;
-    added = addShip(gameBoard, shipLength, x, y, vertical);
-  }
-}
+login(token, onLogin); // Create a connection to Facebook.
 
 /**
- * Checks if  (x, y) is valid for a ship of length shipLength.
- * @param  Number gameBoard  The board which will be checked.
- * @param  Number shipLength The length of the ship to be added.
- * @param  Number x          X coordinate of the ship.
- * @param  Number y          Y coordinate of the ship.
- * @return Boolean           True if the position is valid.
- */
-function checkShipX(gameBoard, shipLength, x, y) {
-  var valid = true;
-  var max = Constants.GAME_BOARD_SIZE - 1;
-  for(var i = 0; i < shipLength; i++) {
-    if(y + i > max || gameBoard[x][y + i] > 0) {
-      valid = false; // Position out of bounds or intersects another ship.
-    }
-  }
-  return valid;
-}
-
-/**
- * Adds a ship to the gameBoard at (x, y) in a horizontal orientation.
- * @param Number gameBoard  The board to add the ship to.
- * @param Number shipLength The length of the ship.
- * @param Number x          X coordinate of the ship.
- * @param Number y          Y coordinate of the ship.
- */
-function addShipX(gameBoard, shipLength, x, y) {
-  for(var i = 0; i < shipLength; i++) {
-    gameBoard[x][y + i] = shipLength;
-  }
-}
-
-/**
- * Checks if  (x, y) is valid for a ship of length shipLength.
- * @param  Number gameBoard  The board which will be checked.
- * @param  Number shipLength The length of the ship to be added.
- * @param  Number x          X coordinate of the ship.
- * @param  Number y          Y coordinate of the ship.
- * @return Boolean           True if the position is valid.
- */
-function checkShipY(gameBoard, shipLength, x, y) {
-  var valid = true;
-  var max = Constants.GAME_BOARD_SIZE - 1;
-  for(var i = 0; i < shipLength; i++) {
-    if(x + i > max || gameBoard[x + i][y] > 0) {
-      valid = false; // Position out of bounds or intersects another ship.
-    }
-  }
-  return valid;
-}
-
-/**
- * Adds a ship to the gameBoard at (x, y) in a vertical orientation.
- * @param Number gameBoard  The board to add the ship to.
- * @param Number shipLength The length of the ship.
- * @param Number x          X coordinate of the ship.
- * @param Number y          Y coordinate of the ship.
- */
-function addShipY(gameBoard, shipLength, x, y) {
-  for(var i = 0; i < shipLength; i++) {
-    gameBoard[x + i][y] = shipLength;
-  }
-}
-
-/**
- * Adds a ship to gameBoard at (x, y), if the position is valid.
- * @param  Number gameBoard  The board to add the ship to.
- * @param  Number shipLength The length of the ship.
- * @param  Number x          X coordinate of the ship.
- * @param  Number y          Y coordinate of the ship.
- * @param  Boolean vertical  If true, ship is placed vertically, horizontal otherwise.
- * @return Boolean           True if the ship was placed, false otherwise.
- */
-function addShip(gameBoard, shipLength, x, y, vertical) {
-  var valid = false;
-  if(vertical) {
-    valid = checkShipY(gameBoard, shipLength, x, y); // Check if location is valid.
-    if(valid) {
-      addShipY(gameBoard, shipLength, x, y); // If so, add the ship.
-    }
-  } else {
-    valid = checkShipX(gameBoard, shipLength, x, y); // Check if location is valid.
-    if(valid) {
-      addShipX(gameBoard, shipLength, x, y); // If so, add the ship.
-    }
-  }
-  return valid;
-}
-
-/**
- * Returns a random integer between min and max.
- * @param  Number min The lower bound of possible integers.
- * @param  Number max The upper bound of possible integers.
- * @return Number     A random integer between min and max.
- */
-function getRandomInt(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-/**
- * Creates a new game board, and initializes all elements to zero.
- * @return Integer array representing a game board.
- */
-function createGameBoard() {
-  var x = new Array(Constants.GAME_BOARD_SIZE);
-  for (var i = 0; i < x.length; i++) {
-    x[i] = new Array(Constants.GAME_BOARD_SIZE);
-    x[i].fill(0); // Fill the empty board with zeroes.
-  }
-  return x;
-}
-
-/**
- * [onLogin description]
- * @param  {[type]} err [description]
- * @param  {[type]} api [description]
- * @return {[type]}     [description]
+ * Fired after a successful login. Registers event handler for events.
+ * @param  Object err Contains the stack and trace associated with any errors.
+ * @param  Object api A reference to the Facebook api.
+ * @return Object     Returns after an error has occured.
  */
 function onLogin(err, api) {
-  if(err) {
-    return console.error(err);
-  }
-  api.setOptions(loginPrefs);
+  if(err) return console.error(err);
+  api.setOptions(prefs);
   api.listen(function callback(err, message) {
-    onEventReceived(api, err, message);
+    onEventReceived(api, err, message); // Handle event.
   });
 }
 
 /**
- * [onEventReceived description]
- * @param  {[type]} api     [description]
- * @param  {[type]} err     [description]
- * @param  {[type]} message [description]
- * @return {[type]}         [description]
+ * Fired after an event is received. Fires other appropriate events.
+ * @param  Object api     A reference to the Facebook api.
+ * @param  Object err     Contains the stack and trace associated with any errors.
+ * @param  Object event   Contains metadata for an event such as date, time, and body.
+ * @return Object         Returns after an error has occured.
  */
-function onEventReceived(api, err, message) {
-  if(err) {
-    return console.log(err);
-  }
-  switch(message.type) {
-    case 'message':
-
-      var body = message.body.toLowerCase(); //Sanitize input
-
-      if(body.startsWith("/begingame")){
-        var g = new Game();
-        g.isStarted = false;
-        g.opponentID = message.senderID;
-        g.gameID = 17;
-        g.threadID = message.threadID;
-        g.api = api;
-        g.beginGame();
-        //api.sendMessage("Your game ID is " + g.gameID + " and your board looks like " + g.playerGameBoard, message.threadID);
-
-        games[message.senderID] = g;
-      } else if(body.startsWith("/help")) {
-        api.sendMessage('The command "/begingame" will start your battleship game!', message.threadID);
-      } else {
-        var g = games[message.senderID];
-        g.messageReceive(message.body);
-      }
-
-      insertMessage(message);
-      api.markAsRead(message.threadID, function(err) {
-        if(err) {
-          console.error(err);
-        }
-      });
-      break;
-    case 'event':
-      console.log(message);
-      break;
+function onEventReceived(api, err, event) {
+  if(err) return console.error(err);
+  if(event.type == 'message') {
+    onMessageReceived(api, event);
+  } else if(event.type == 'event') {
+    console.log(event);
   }
 }
 
 /**
- * [insertMessage description]
- * @param  {[type]} message [description]
- * @return {[type]}         [description]
+ * Fired after a message is received. Passes relevant information to games.
+ * @param  Object api     A reference to the Facebook api
+ * @param  Object message Contains metadata for a message such as date, time, and body.
+ * @return Object         Returns after an error has occured.
+ */
+function onMessageReceived(api, message) {
+  var body = message.body.toLowerCase(); //Sanitize input.
+  insertMessage(body);
+  markMessageAsRead(api, message);
+  if(body.startsWith(Constants.COMMAND_BEGIN)) {
+    var game = new Game(message.senderID, message.threadID, api, 17);
+    games[message.senderID] = game; // Store this in the dictionary.
+    game.beginGame(); // Start the game!
+  } else if(body.startsWith(Constants.COMMAND_HELP)) {
+    api.sendMessage(Constants.MESSAGE_HELP, message.threadID);
+  } else {
+    var game = games[message.senderID];
+    game.messageReceive(message.body);
+  }
+}
+
+/**
+ * Marks the specified message as read.
+ * @param  Object api     A reference to the Facebook api
+ * @param  Object message Contains metadata for a message such as date, time, and body.
+ * @return Object         Returns after an error has occured.
+ */
+function markMessageAsRead(api, message) {
+  api.markAsRead(message.threadID, function(err) {
+    if(err) return console.error(err); // Mark message as read on Facebook chat.
+  });
+}
+
+/**
+ * Records the specified message to the database.
+ * @param  Object message Contains metadata for a message such as date, time, and body.
+ * @return Object         Returns after an error has occured.
  */
 function insertMessage(message) {
   db.serialize(function() {
     var stmt = db.prepare("INSERT INTO messages (threadID, messageID, body) VALUES (?, ?, ?)");
-    stmt.run(message.threadID, message.messageID, message.body);
+    stmt.run(message.threadID, message.messageID, message.body); // Fill in template values
     stmt.finalize();
   });
 }
