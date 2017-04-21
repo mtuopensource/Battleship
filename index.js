1b758d64aed3ef8720786c5f1c8a6100e94c127e
Austin <ajwgeek@gmail.com>
2017-04-20 22:00:26 -0400

    Added documentation for the random ship generator.

--- index.js
+++ index.js
@@ -44,67 +44,116 @@ function Game() {
 }
 
 Game.prototype.beginGame = function() {
-  var gameBoard = this.computerGameBoard;
-  addShip(gameBoard, 5);
-  addShip(gameBoard, 4);
-  addShip(gameBoard, 3);
-  addShip(gameBoard, 3);
-  addShip(gameBoard, 2);
+  this.addComputerShip(5);
+  this.addComputerShip(4);
+  this.addComputerShip(3);
+  this.addComputerShip(3);
+  this.addComputerShip(2);
 }
 
-function addShip(board, shipLength) {
-  var placing = true;
-  while(placing) {
+Game.prototype.addComputerShip = function(shipLength) {
+  var added = false;
+  var gameBoard = this.computerGameBoard;
+  while(added == false) {
     var x = getRandomInt(0, 9);
     var y = getRandomInt(0, 9);
     var vertical = getRandomInt(0, 1) == 1;
-    if(vertical) {
-      var invalid = false;
-      for(var i = 0; i < shipLength; i++) {
-        if(y + i > 9) {
-          invalid = true;
-          break;
-        }
-        if(board[x][y + i] > 0) {
-          invalid = true;
-          break;
-        }
-      }
-      if(invalid) {
-        continue;
-      }
-      for(var i = 0; i < shipLength; i++) {
-        board[x][y + i] = shipLength;
-      }
-      placing = false;
-    } else {
-      var invalid = false;
-      for(var i = 0; i < shipLength; i++) {
-        if(x + i > 9) {
-          invalid = true;
-          break;
-        }
-        if(board[x + i][y] > 0) {
-          invalid = true;
-          break;
-        }
-      }
-      if(invalid) {
-        continue;
-      }
-      for(var i = 0; i < shipLength; i++) {
-        board[x + i][y] = shipLength;
-      }
-      placing = false;
-    }
+    added = addShip(gameBoard, shipLength, x, y, vertical);
   }
 }
 
 /**
- * [getRandomInt description]
- * @param  {[type]} min [description]
- * @param  {[type]} max [description]
- * @return {[type]}     [description]
+ * Checks if  (x, y) is valid for a ship of length shipLength.
+ * @param  Integer gameBoard  The board which will be checked.
+ * @param  Integer shipLength The length of the ship to be added.
+ * @param  Integer x          X coordinate of the ship.
+ * @param  Integer y          Y coordinate of the ship.
+ * @return Boolean            True if the position is valid.
+ */
+function checkShipX(gameBoard, shipLength, x, y) {
+  var valid = true;
+  for(var i = 0; i < shipLength; i++) {
+    if(x + i > 9 || gameBoard[x + i][y] > 0) {
+      valid = false; // Position out of bounds or intersects another ship.
+    }
+  }
+  return valid;
+}
+
+/**
+ * Adds a ship to the gameBoard at (x, y) in a horizontal orientation.
+ * @param Integer gameBoard  The board to add the ship to.
+ * @param Integer shipLength The length of the ship.
+ * @param Integer x          X coordinate of the ship.
+ * @param Integer y          Y coordinate of the ship.
+ */
+function addShipX(gameBoard, shipLength, x, y) {
+  for(var i = 0; i < shipLength; i++) {
+    gameBoard[x + i][y] = shipLength;
+  }
+}
+
+/**
+ * Checks if  (x, y) is valid for a ship of length shipLength.
+ * @param  Integer gameBoard  The board which will be checked.
+ * @param  Integer shipLength The length of the ship to be added.
+ * @param  Integer x          X coordinate of the ship.
+ * @param  Integer y          Y coordinate of the ship.
+ * @return Boolean            True if the position is valid.
+ */
+function checkShipY(gameBoard, shipLength, x, y) {
+  var valid = true;
+  for(var i = 0; i < shipLength; i++) {
+    if(y + i > 9 || gameBoard[x][y + i] > 0) {
+      valid = false; // Position out of bounds or intersects another ship.
+    }
+  }
+  return valid;
+}
+
+/**
+ * Adds a ship to the gameBoard at (x, y) in a vertical orientation.
+ * @param Integer gameBoard  The board to add the ship to.
+ * @param Integer shipLength The length of the ship.
+ * @param Integer x          X coordinate of the ship.
+ * @param Integer y          Y coordinate of the ship.
+ */
+function addShipY(gameBoard, shipLength, x, y) {
+  for(var i = 0; i < shipLength; i++) {
+    gameBoard[x][y + i] = shipLength;
+  }
+}
+
+/**
+ * Adds a ship to gameBoard at (x, y), if the position is valid.
+ * @param  Integer gameBoard  The board to add the ship to.
+ * @param  Integer shipLength The length of the ship.
+ * @param  Integer x          X coordinate of the ship.
+ * @param  Integer y          Y coordinate of the ship.
+ * @param  Boolean vertical   If true, ship is placed vertically, horizontal otherwise.
+ * @return Boolean            True if the ship was placed, false otherwise.
+ */
+function addShip(gameBoard, shipLength, x, y, vertical) {
+  var valid = false;
+  if(vertical) {
+    valid = checkShipY(gameBoard, shipLength, x, y); // Check if location is valid.
+    if(valid) {
+      addShipY(gameBoard, shipLength, x, y); // If so, add the ship.
+    }
+  } else {
+    valid = checkShipX(gameBoard, shipLength, x, y); // Check if location is valid.
+    if(valid) {
+      addShipX(gameBoard, shipLength, x, y); // If so, add the ship.
+    }
+  }
+  return valid;
+}
+
+/**
+ * Returns a random integer between min and max.
+ * @param  Integer min The lower bound of possible integers.
+ * @param  Integer max The upper bound of possible integers.
+ * @return Integer     A random integer between min and max.
  */
 function getRandomInt(min, max) {
   return Math.round(Math.random() * (max - min) + min);
