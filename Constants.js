2222f9482a823b14f44892c5f19554ce9d4e2a96
Austin <ajwgeek@gmail.com>
2017-04-21 02:19:57 -0400

    Added additional options to Constants.js

---
+++ Constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+  FILE_PREFS: '/secrets.env',
+  FILE_DB:    'games.sqlite',
+  GAME_BOARD_SIZE: 10,
+  SHIPS: {
+    "Carrier": 5,
+    "Battleship": 4,
+    "Cruiser": 3,
+    "Submarine": 3,
+    "Destroyer": 2
+  },
+  COMMAND_BEGIN: '/begin',
+  COMMAND_HELP: '/help',
+  MESSAGE_HELP: 'The command "/begingame" will start your battleship game!'
+};
